4b55660af7ccd0b2fc66f84ab46d20458a964c98
Peyton Nguyen <peytonn72@gmail.com>
2024-10-04 22:33:55 -0700

    add monaco editor

--- apps/web/app/routes/problems.tsx
+++ apps/web/app/routes/problems.tsx
@@ -1,8 +1,15 @@
-export function Problems() {
+import { Editor } from '@monaco-editor/react';
+
+export default function Problems() {
   return (
-    <div>
+    <div className="w-full flex flex-col h-full p-12">
       <h1>List of problems</h1>
       <p>Problem</p>
+      <Editor
+        defaultLanguage="python"
+        defaultValue="// some comment"
+        theme="vs-dark"
+      />
     </div>
   );
 }
